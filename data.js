6105a9b9dcd6a2ce85ce15c02d6ce3a00627f77f
David Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2023-10-04 12:13:52 +0100

    Created film genres array of objects.

---
+++ data.js
@@ -0,0 +1,24 @@
+'use strict';
+
+export const filmGenres = 
+[
+    {genre: "Horror",
+    films: 
+    ['Hellraiser', 'Alien', 'Evil dead'] },
+
+    {genre: "Science Fiction",
+    films: 
+    ['Gravity', 'Avatar', 'Matrix'] },
+
+    {genre: "Action",
+    films: 
+    ['Nobody', 'Peppermint', 'Expendables'] },
+
+    {genre: "Comedy",
+    films: 
+    ['Central Intelligence', 'Cocaine Bear', 'Tropic Thunder'] },
+
+    {genre: "Romance",
+    films: 
+    ['The Age of Adaline', 'Safe Haven', 'About Time'] },
+];
